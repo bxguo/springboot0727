5dbe4021a0419a7701b783f2e4e6a7f0c768991c
bxguo <951409002@qq.com>
2018-09-05 17:40:46 +0800

    proc：mongdb 模块初始化

---
+++ bxguo-mongdb/src/main/java/bxguo/mongdb/demo/BxguoMongdbApplication.java
@@ -0,0 +1,12 @@
+package bxguo.mongdb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxguoMongdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BxguoMongdbApplication.class, args);
+    }
+}
